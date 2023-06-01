7141b1357bf17f68a7ac08db050d647dbeba0889
Anton <B_atonyo_S@bk.ru>
2023-06-01 17:37:45 +0300

    Task 30 String.Join

--- Task 30/Program.cs
+++ Task 30/Program.cs
@@ -4,25 +4,28 @@
 // int a = new Random().Next(2, 7);
 // int b = new Random().Next(2);
 // Console.WriteLine($"a = {a},b = {b}");
-int Number(){
-Console.Clear();
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine());
-return number;
-}
-int [] Array_Random(int n)
+int Number()
 {
-    int [] array = new int[n];
-    for ( int i = 0; i<n; i++)
-        array[i] = new Random().Next(0,2);
-       return array;
+    Console.Clear();
+    Console.WriteLine("Введите число");
+    int number = int.Parse(Console.ReadLine());
+    return number;
 }
-void Print_Array(int [] array){
-    for (int i=0; i<array.Length; i++) Console.Write($"{array[i]} ");
+int[] Array_Random(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+        array[i] = new Random().Next(0, 2);
+    return array;
+}
+void Print_Array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
 }
 int number = Number();
-int [] array = Array_Random(number);
+int[] array = Array_Random(number);
 Print_Array(array);
+Console.WriteLine($"[{String.Join("; ---", array)}]");// - выводит массив array
 Console.WriteLine();
 
 

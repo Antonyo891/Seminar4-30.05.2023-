0caaded2a6e256f09a3e41d1bef7812090ed3d9f
Anton <B_atonyo_S@bk.ru>
2023-06-01 08:46:35 +0300

    Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Программа принимает на ввод число А и выдает сумму от 1 до А (метод)
+int GetSum(int n){
+int sum = 0;
+for (int i=1; i<=n; i++)
+sum +=i;// == sum = sum + i
+return sum;
+}
+Console.Clear();
+Console.WriteLine("Введите число");
+int Number = int.Parse(Console.ReadLine());
+Console.WriteLine($"{GetSum(Number)}");

5cb7c4629c10f4706f9d95752d5af1bb90ddf823
Anton <B_atonyo_S@bk.ru>
2023-06-01 09:34:45 +0300

    Task 28 completed

---
+++ Task.28/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*//28 Напишите программу, которая принимает на вход число N
+//и выдаёт произведение чисел от 1 до N.
+
+//4-> 24
+//5-> 120 == 1 * 2 * 3 * 4 * 5*/
+int Product(int n){
+    int product = 1;
+    for (int i = 2; i<=Math.Abs(n); i++)
+        product = product*i;
+    return product;
+}
+int Number(){
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+int number = Number();
+System.Console.WriteLine($"Произведение {number} чисел = {Product(number)}");
